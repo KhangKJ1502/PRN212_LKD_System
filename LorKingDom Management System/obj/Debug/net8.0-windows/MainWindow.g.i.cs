e638be9c393695d3778536053151ff23204dd392
KhangKJ1502 <125472792+KhangKJ1502@users.noreply.github.com>
2025-03-15 18:04:03 +0700

    product view

--- LorKingDom Management System/obj/Debug/net8.0-windows/MainWindow.g.i.cs
+++ LorKingDom Management System/obj/Debug/net8.0-windows/MainWindow.g.i.cs
@@ -52,7 +52,7 @@ namespace LorKingDom_Management_System {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/LorKingDom Management System;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/LorKingDom Management System;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
