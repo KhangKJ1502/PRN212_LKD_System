e1820ab5d3b132faaa9b899e054ec28677ee0e17
lork18123Ok <ltluana23109@cusc.ctu.edu.vn>
2025-03-15 18:00:15 +0700

    Add Register

--- LorKingDom Management System/obj/Debug/net8.0-windows/Register.g.i.cs
+++ LorKingDom Management System/obj/Debug/net8.0-windows/Register.g.i.cs
@@ -117,7 +117,7 @@ namespace LorKingDom_Management_System {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/LorKingDom Management System;V1.0.0.0;component/register.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/LorKingDom Management System;component/register.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Register.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
